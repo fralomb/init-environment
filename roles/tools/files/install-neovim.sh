874cb22f7db3e54ed0fbcdf956dbfdf710a75216
Francesco Lombardo <fra.lombardo92@gmail.com>
2022-05-22 15:26:42 +0200

    add submodule variable

--- roles/tools/files/install-neovim.sh
+++ roles/tools/files/install-neovim.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/bash
 set -e
 
-HOME="$1"
-TARGET="$2"
-
-[[ -z "$HOME" ]] && echo "Missing home path." && exit 1
-[[ -z "$TARGET" ]] && echo "Missing target path." && exit 1
-
-## install prerequisites
-#apt-get update
-#apt-get install	-y 	ninja-build	\
-#			gettext		\
-#			libtool		\
-#			libtool-bin	\
-#			autoconf	\
-#			automake	\
-#			cmake		\
-#			g++		\
-#			pkg-config	\
-#			unzip		\
-#			curl		\
-#			doxygen
-
-# clone neovim
-ssh-keygen -F github.com || ssh-keyscan github.com >> $HOME/.ssh/known_hosts
-git clone https://github.com/neovim/neovim $TARGET/neovim
-
-# build stable version
-cd $TARGET/neovim
 git checkout stable
 make -j4
 
